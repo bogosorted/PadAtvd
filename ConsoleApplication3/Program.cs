f023a1552484b7b296e0565ab68958da790cbe46
Gabriel A. & Gabriel D <aluno@naverecife.local>
2019-03-29 11:38:51 -0300

    Atividade PAD

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAD2C032919
+{
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            SUBORDINADOS promovido = new SUBORDINADOS();
+            Console.WriteLine("Parabéns você foi PRO-MO-VI-DO!!!");
+            Console.Write("Escreva o seu primeiro nome:");
+            string nome = Console.ReadLine();
+            promovido.DefinirNome(nome);
+        }
+    }
+}
---
+++ ConsoleApplication3/SUBORDINADOS.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAD2C032919
+{
+    class SUBORDINADOS
+    {
+        public string primeiroNome;
+        public string psobrenome;
+        public float salarioPmes;
+        public float aumento = 0.1f;
+
+        public float AtualizacaoPromo()
+        {
+            return salarioPmes += aumento * salarioPmes;
+        }
+        public string DefinirNome(string nome)
+        {    
+            return primeiroNome = nome;
+        }
+        public string DefinirSobrenome(string sobrenome)
+        {
+            return psobrenome = sobrenome;
+        }
+        public float DefinirSalarioMensal(float Salario)
+        {
+            return salarioPmes = Salario;
+        }
+    }
+}
